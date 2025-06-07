a2a737e6fd3831963cca50545b0930cc28407ac6
Saeb Naser <56490771+Saeb0x@users.noreply.github.com>
2025-06-08 00:57:48 +0800

    Added a TRACE log level and adjusted the log formatting.

--- Afterglow/src/Afterglow/Core/Log.cpp
+++ Afterglow/src/Afterglow/Core/Log.cpp
@@ -10,11 +10,11 @@ namespace Afterglow
 
 	void Log::Init()
 	{
-		spdlog::set_pattern("%^%r [%n]: %l: %v%$");
+		spdlog::set_pattern("%r [%n]: %^[%l]%$: %v");
 
 		s_CoreLogger = spdlog::stdout_color_mt("AFTERGLOW");
 		s_ClientLogger = spdlog::stdout_color_mt("APP");
 
-		spdlog::set_level(spdlog::level::info);
+		spdlog::set_level(spdlog::level::trace);
 	}
 }
--- Afterglow/src/Afterglow/Core/Log.h
+++ Afterglow/src/Afterglow/Core/Log.h
@@ -27,18 +27,22 @@ namespace Afterglow
 }
 
 #ifdef AG_SHIPPING
+	#define AG_LOG_TRACE(...)
 	#define AG_LOG_INFO(...) 
 	#define AG_LOG_WARNING(...) 
 	#define AG_LOG_ERROR(...)
 	
+	#define AG_APP_LOG_TRACE(...)
 	#define AG_APP_LOG_INFO(...) 
 	#define AG_APP_LOG_WARNING(...) 
 	#define AG_APP_LOG_ERROR(...)
 #else
+	#define AG_LOG_TRACE(...) ::Afterglow::Log::GetCoreLogger()->trace(__VA_ARGS__)
 	#define AG_LOG_INFO(...) ::Afterglow::Log::GetCoreLogger()->info(__VA_ARGS__)
 	#define AG_LOG_WARNING(...) ::Afterglow::Log::GetCoreLogger()->warn(__VA_ARGS__)
 	#define AG_LOG_ERROR(...) ::Afterglow::Log::GetCoreLogger()->error(__VA_ARGS__)
 	
+	#define AG_APP_LOG_TRACE(...) ::Afterglow::Log::GetClientLogger()->trace(__VA_ARGS__)
 	#define AG_APP_LOG_INFO(...) ::Afterglow::Log::GetClientLogger()->info(__VA_ARGS__)
 	#define AG_APP_LOG_WARNING(...) ::Afterglow::Log::GetClientLogger()->warn(__VA_ARGS__)
 	#define AG_APP_LOG_ERROR(...) ::Afterglow::Log::GetClientLogger()->error(__VA_ARGS__)
